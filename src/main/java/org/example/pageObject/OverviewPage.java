7c6eec6e683f353773f84cc5cef79c9b4d54614c
alfianadityads <alfianadityadwi@gmail.com>
2023-09-07 23:18:56 +0700

    chore: setup webdriver for init element

--- src/main/java/org/example/pageObject/OverviewPage.java
+++ src/main/java/org/example/pageObject/OverviewPage.java
@@ -1,5 +1,14 @@
 package org.example.pageObject;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class OverviewPage {
-    
+
+    public static WebDriver driver;
+
+    public OverviewPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        OverviewPage.driver = driver;
+    }
 }
