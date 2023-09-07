20e64a50a80163a1f20b66654140055e9a83ec02
alfianadityads <alfianadityadwi@gmail.com>
2023-09-08 00:10:59 +0700

    chore: connect to overview page

---
+++ src/test/java/step_definitions/OverviewSteps.java
@@ -0,0 +1,13 @@
+package step_definitions;
+
+import org.example.pageObject.OverviewPage;
+import org.openqa.selenium.WebDriver;
+
+public class OverviewSteps {
+
+    private WebDriver driver = Hooks.driver;
+
+    OverviewPage overviewPage = new OverviewPage(driver);
+
+    
+}
